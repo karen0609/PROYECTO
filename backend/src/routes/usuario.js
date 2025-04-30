ac8d3ee7c49348e2cb54c1b1a096ce12add27c1d
Karen Nahomi Zurita Lopez <nahomi.zurita@unach.edu.ec>
2025-04-30 17:03:32 -0500

    arreglos

--- backend/src/routes/usuario.js
+++ backend/src/routes/usuario.js
@@ -2,13 +2,13 @@ const {Router} = require('express');
 const { get } = require('mongoose');
 const router = Router();
 
+const { getUsu, createUsu, getUsuario, deleteUsu, updateUsu } = require('../controller/usuario.controller.js');
+
 router.route('/')  
-        .get()
-        .post()
+        .get(getUsu)
+        .post(createUsu)        
 
 router.route('/:id')
-        .get()
-        .delete()
-        .put()
-
-module.exports = router;
+        .get(getUsuario)
+        .delete(deleteUsu)
+        .put(updateUsu)
